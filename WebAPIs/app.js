dab8d32577d37588d287558c2d7da656d5d39380
kuya-Ey <138701056+kuya-Ey@users.noreply.github.com>
2023-09-02 22:22:35 +0800

    started coding for nodejs activity
    copied most of the framework codes from the recent lesson, somehow I couldnt run it on my machine. pushing this for references,its at dev branch

--- WebAPIs/app.js
+++ WebAPIs/app.js
@@ -1,4 +1,4 @@
-// genres-movielist 'GET'
+// // genres-movielist 'GET'
 
 // const data = null;
 
@@ -18,26 +18,27 @@
 // xhr.send(data);
 
 // GET method
-let button = document.getElementById('button');
+// let button = document.getElementById('button');
 
-// create an XMLHttpRequest object
-const req = new XMLHttpRequest();
+// // create an XMLHttpRequest object
+// const req = new XMLHttpRequest();
 
-// define a callback function, this case with an eventListener
-button.addEventListener('click', function(){
-    if(req.status === 200 && req.readyState === 4){
-        const res = JSON.parse(req.responseText);
-        console.log(res);
-    }else{
-        throw new Error("Something went wrong!");
-        // console.log("Something went wrong!");
-    }
-});
 
-// send a request to the server
-req.open('GET', 'https://api.themoviedb.org/3/genre/movie/list?language=en');
+// // define a callback function, this case with an eventListener
+// button.addEventListener('click', function(){
+//     if(req.status === 200 && req.readyState === 4){
+//         const res = JSON.parse(req.responseText);
+//         console.log(res);
+//     // }else{
+//     //     throw new Error("Something went wrong!");
+//     //     console.log("Something went wrong!");
+//     }
+// });
 
-req.send();
+// // send a request to the server
+// req.open('GET', 'https://api.themoviedb.org/3/genre/movie/list?language=en');
+
+// req.send();
 
 //POST request (Lists - Create: ) for exploring
 // const req = new XMLHttpRequest();
@@ -63,3 +64,39 @@ req.send();
 
 // req.send(JSON.stringify(newUser));
 
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+button.addEventListener('click', function () {
+	if(xhr.status === 200 && xhr.readyState === 4) {
+        const res = JSON.parse(xhr.responseText);
+		console.log(xhr.responseText);
+	}else{
+        throw new Error('Something went wrong!');
+        // console.log(Something went wrong!);
+    }
+});
+
+xhr.open('GET', 'https://odds.p.rapidapi.com/v4/sports');
+xhr.setRequestHeader('X-RapidAPI-Key', '5cd923a73emshdcd724e0bc24203p1093aejsndb948d99de7b');
+xhr.setRequestHeader('X-RapidAPI-Host', 'odds.p.rapidapi.com');
+xhr.send(data);
+
+let button = document.getElementById('button');
+
+const ret = new XMLHttpRequest();
+
+button.addEventListener("click", function () {
+    if(ret.status === 200 && ret.readyState === 4) {
+        const res = JSON.parse(ret.responseText);
+        console.log(res.responseText);
+    }else{
+        throw new Error('Something went wrong!');
+    }
+});
+
+ret.open("GET", "https://odds.p.rapidapi.com/v4/sports");
+
+ret.send();
---
+++ node.js/app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const bcrypt = require('bcrypt');
+
+const hostname = '127.0.0.1';
+const port = 5500;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end('Hello Madlang People, Mabuhay!');
+
+});
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server running at port ${hostname}:${port}`);
+});
+
